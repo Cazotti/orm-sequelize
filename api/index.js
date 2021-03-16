a8973af15e0f05c0173db84623986b84771dad3e
Victor Cazotti <cazotti.victor@gmail.com>
2021-03-16 11:38:19 -0300

    Complete module 03, controllers and routes.

--- api/index.js
+++ api/index.js
@@ -1,13 +1,10 @@
 const express = require('express')
+const routes = require('./routes')
 
 const app = express()
 const port = 3000
 
-app.use(express.json())
-
-app.get('/', (req, res) => res
-  .status(200)
-  .send({ mensagem: 'Boas vindas à API' }))
+routes(app)
 
 app.listen(port, () => console.log(`Server running at port ${port}`))
 
